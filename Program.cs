e4e6c44d4bcfad6f37217287aed842934dccd3bb
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-12-18 16:52:35 +0100

    Works well, console doesn't write Japanese, might work in Unity

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        TraslationEffect("これは非現実的なテストです", "This is an impractical test");
+    }
+    static void TraslationEffect(string original, string translated)
+    {
+        Console.SetCursorPosition((Console.WindowWidth - original.Length) / 2, Console.CursorTop);
+        Console.Write(original);
+
+        Console.SetCursorPosition((Console.WindowWidth - translated.Length) / 2, Console.CursorTop);
+
+        foreach (char c in translated)
+        {
+            Thread.Sleep(100);
+            Console.Write(c);
+        }
+    }
+}
